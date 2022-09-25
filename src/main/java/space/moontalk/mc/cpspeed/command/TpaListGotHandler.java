6b4a2f8096c56768561e3df55765ba9d6b759681
Maksim Victorovich Fomin <fominmv2498@yandex.ru>
2022-09-25 16:17:28 +0300

    Implemented request listing

--- src/main/java/space/moontalk/mc/cpspeed/command/TpaListGotHandler.java
+++ src/main/java/space/moontalk/mc/cpspeed/command/TpaListGotHandler.java
@@ -5,6 +5,8 @@ import org.bukkit.entity.Player;
 
 import org.jetbrains.annotations.NotNull;
 
+import lombok.val;
+
 import space.moontalk.mc.cpspeed.teleport.TeleportManager;
 
 // list (got | in) %p?
@@ -16,11 +18,41 @@ public class TpaListGotHandler extends AbstractTpaListHandler {
 
     @Override
     protected void handleSpecifiedPlayer(@NotNull CommandSender sender, @NotNull Player player) {
+        val requests        = teleportManager.getGotRequests(player);
+        val messageProvider = teleportManager.getMessageProvider();
+        
+        if (requests.isEmpty()) {
+            val message = messageProvider.makeHasNoGotRequestsMessage(player);
+            sender.sendMessage(message);
+            return;
+        }
 
+        var message = messageProvider.makeGotRequestsHeaderMessage(player, requests);
+        sender.sendMessage(message);
+
+        for (val request : requests) {
+            message = messageProvider.makeGotRequestsItemMessage(request, player);
+            sender.sendMessage(message);
+        }
     }
 
     @Override
     protected void handleSenderPlayer(@NotNull Player player) {
+        val requests        = teleportManager.getGotRequests(player);
+        val messageProvider = teleportManager.getMessageProvider();
+        
+        if (requests.isEmpty()) {
+            val message = messageProvider.makeYouHaveNoGotRequestsMessage(player);
+            player.sendMessage(message);
+            return;
+        }
 
+        var message = messageProvider.makeYourGotRequestsHeaderMessage(player, requests);
+        player.sendMessage(message);
+
+        for (val request : requests) {
+            message = messageProvider.makeGotRequestsItemMessage(request, player);
+            player.sendMessage(message);
+        }
     }
 }
